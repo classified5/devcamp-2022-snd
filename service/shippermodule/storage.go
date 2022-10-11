e03aefe8c189dfaa13c39241ca7e49af7279c477
Kevin Sunjaya <6181901008@student.unpar.ac.id>
2022-10-11 23:45:30 +0700

    add delete feature, kalau id nya ga ketemu, cuman bisa keluar di log karena format model nya gabisa masukin message(?)

--- service/shippermodule/storage.go
+++ service/shippermodule/storage.go
@@ -90,6 +90,30 @@ func (s *storage) GetShipperAll(ctx context.Context) (result []m.ShipperResponse
 	return
 }
 
+func (s *storage) DeleteShipper(ctx context.Context, id int64, param m.ShipperRequest) (result m.ShipperResponse, err error) {
+	res, err := s.ShipperDB.ExecContext(ctx, deleteShipperQuery,
+		id,
+	)
+
+	if err != nil {
+		log.Println("[ShipperModule][DeleteShipper][Storage] problem querying to db, err: ", err.Error())
+		return
+	}
+
+	rowsAffected, err := res.RowsAffected()
+	if err != nil {
+		log.Println("[ShipperModule][DeleteShipper] problem querying to db, err: ", err.Error())
+		return
+	}
+	if rowsAffected == 0 {
+		log.Println("[ShipperModule][DeleteShipper] no rows affected in db")
+		return
+	}
+
+	result.ID = id
+	return
+}
+
 func (s *storage) UpdateShipper(ctx context.Context, id int64, param m.ShipperRequest) (result m.ShipperResponse, err error) {
 	res, err := s.ShipperDB.ExecContext(ctx, updateShipperQuery,
 		param.Name,
@@ -113,8 +137,8 @@ func (s *storage) UpdateShipper(ctx context.Context, id int64, param m.ShipperRe
 		return
 	}
 	if rowsAffected == 0 {
-		log.Println("[ShipperModule][UpdateShipper] no rows affected in db")
-		return 
+		log.Println("[ShipperModule][UpdateShipper] ID not found. No rows affected in db")
+		return
 	}
 
 	result.ID = id
