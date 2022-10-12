03141c1289ed4613eef7b747503fea150095ccaf
Edward Suryaputra <edward.suryaputra@aux.dkatalis.com>
2022-10-12 07:15:55 +0700

    DB: add seller table and foreign key connection to shipper with seller

--- db/create_tables.sql
+++ db/create_tables.sql
@@ -4,8 +4,20 @@ CREATE TABLE IF NOT EXISTS shipper (
     image_url text NOT NULL,
     description text NOT NULL,
     max_weight int NOT NULL,
+    seller_id varchar NOT NULL,
     created_at timestamp NOT NULL,
     created_by int NOT NULL,
     updated_at timestamp NOT NULL,
-    updated_by int NOT NULL
+    updated_by int NOT NULL,
+    FOREIGN KEY (seller_id) REFERENCES seller(id)
+);
+
+CREATE TABLE IF NOT EXISTS seller (
+    id bigserial PRIMARY KEY,
+    name varchar NOT NULL,
+    password varchar NOT NULL,
+    created_at timestamp NOT NULL,
+    created_by int NOT NULL,
+    updated_at timestamp,
+    updated_by int
 );
