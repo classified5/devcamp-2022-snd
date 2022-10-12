718b9bf49559c7f03a94d19c6057f1b5997bece4
Edward Suryaputra <edward.suryaputra@aux.dkatalis.com>
2022-10-12 09:01:49 +0700

    Feat: add seller endpoints

--- service/main.go
+++ service/main.go
@@ -9,7 +9,9 @@ import (
 	"github.com/gorilla/mux"
 
 	"github.com/classified5/devcamp-2022-snd/service/database"
+	"github.com/classified5/devcamp-2022-snd/service/sellermodule"
 	"github.com/classified5/devcamp-2022-snd/service/server"
+	sellerHandler "github.com/classified5/devcamp-2022-snd/service/server/handlers/seller"
 	shipperHandler "github.com/classified5/devcamp-2022-snd/service/server/handlers/shipper"
 	"github.com/classified5/devcamp-2022-snd/service/shippermodule"
 )
@@ -30,6 +32,7 @@ func main() {
 
 	router := mux.NewRouter()
 
+	initSeller(db, router)
 	initShipper(db, router)
 
 	serverConfig := server.Config{
@@ -58,4 +61,19 @@ func initShipper(db *sql.DB, router *mux.Router) {
 	router.HandleFunc("/shipper/{id}", sh.DeleteShipperHandler).Methods(http.MethodDelete)
 	router.HandleFunc("/shippers", sh.GetShipperAllHandler).Methods(http.MethodGet)
 	router.HandleFunc("/", sh.RootHandler).Methods(http.MethodGet)
+}
+
+func initSeller(db *sql.DB, router *mux.Router) {
+	// Init shipper usecase
+	log.Println("Initializing Usecase")
+	sm := sellermodule.NewSellerModule(db)
+
+	// Init shipper handler
+	log.Println("Initializing Handler")
+	sh := sellerHandler.NewSellerHandler(sm)
+
+	// REST Handlers
+	router.HandleFunc("/seller", sh.AddSellerHandler).Methods(http.MethodPost)
+	router.HandleFunc("/seller/{id}", sh.GetSellerHandler).Methods(http.MethodGet)
+	router.HandleFunc("/sellers", sh.GetSellerAllHandler).Methods(http.MethodGet)
 }
